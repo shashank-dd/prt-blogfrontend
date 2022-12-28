0e339f0c405cbcdaf112982994188531f481cb32
shashank-dd <shashankdd1805@gmail.com>
2022-12-28 20:05:24 +0530

    added

---
+++ src/component/create.js
@@ -0,0 +1,45 @@
+import React, {  useEffect, useState } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import axios from "axios";
+import './create.css';
+
+const Create = () => {
+    let navigate = useNavigate()
+const [post,setpost]=useState({ title:"",image:"",description:""})
+const location =useLocation();
+useEffect(()=>{
+    console.log(777)
+    console.log(location.state.name)
+ setpost({
+    name:location.state.name,
+    ...post
+ })
+},[])
+async function hh() {
+    const config = {
+        headers: {
+            "content-type": "multipart/form-data"
+        }
+    }
+    const data = await axios.post("http://localhost:8080/blog/post", post, config)
+    console.log(data)
+    if (data.data.ok === "ok") {
+        navigate("/homepage")
+    
+    }
+}
+
+  return (
+    <div className="ba">
+      <div><input type="text" onChange={(e)=>{ setpost({...post,title:e.target.value}) }}></input></div>
+      <div><input type="file"onChange={(e)=>{ setpost({...post,image:e.target.files[0]}) }} ></input></div>
+      <div><input type="text" onChange={(e)=>{ setpost({...post,description:e.target.value}) }}></input></div>
+      <div><button onClick={hh}>post</button></div>
+     
+      
+    </div>
+  )
+}
+
+
+export default Create;
---
+++ src/component/homepage.js
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react';
+import './homepage.css';
+import { useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import axios from "axios";
+function HomePage() {
+
+  const [dta, setdta] = useState([])
+  const [name, setname] = useState("")
+
+
+  let navigate = useNavigate()
+  useEffect(() => {
+    axios.post("http://localhost:8080/blog/data", { token: window.localStorage.getItem("token") }).then(response => {
+      console.log(response.data.dat)
+      setdta(response.data.dat)
+      console.log("dta", dta)
+      console.log(response.data.user)
+      setname(response.data.user)
+      // setgl(response.data.userid)
+      // setta(response.data.dat)
+    }).catch(error => { console.log(error, 11111111) })
+  }, [])
+
+   let logoutHandler = () =>{
+      console.log(1)
+              window.localStorage.removeItem('token')
+
+              navigate("/")
+           }
+  return (
+    <div className='homepage'>
+      <div id='nav'>
+        <span>Blog LoGo</span>
+       <Link to="/homepage"><span id='home'>home</span></Link> 
+        <Link to="/create" state={{ name: name }}> <span id='create'>create</span> </Link>
+        <span id='log' onClick={logoutHandler}>Logout</span>
+
+      </div>
+      <div id='body'>
+        {dta && dta.map((data) => {
+          return <div id='ll'>
+            <div className='l1'><img src={data.image}></img></div>
+            <div className='l1' >
+              <h2 className='red'  > Title  : {data.title}</h2>
+              <p className='blue' id='bl' >description</p>
+              <p className='blue'>{data.description}</p>
+              <p className='green'> by: {name}</p>
+            </div>
+          </div>
+        })}
+
+
+      </div>
+
+    </div>
+  )
+}
+
+
+export default HomePage;
